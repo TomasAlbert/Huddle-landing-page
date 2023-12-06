b9dc353bf2a1fd43b474e231305189c9690ca6c3
TomasAlbert <tomasalbert9387182@gmail.com>
2023-12-06 19:55:08 +0100

    feat: main styled structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,25 @@
 module.exports = {
 	content: ["./*.{html,js}"],
 	theme: {
+		extend: {
+			backgroundImage: {
+				HeroPatternDesktop: "url('../images/bg-desktop.svg')",
+				HeroPatternMobile: "url('../images/bg-mobile.svg')",
+			},
+		},
 		screens: {
 			sm: "480px",
 			md: "768px",
-			lg: "976px",
-			xl: "1440px",
+			lg: "1300px",
+			xl: "1400px",
 		},
-
 		colors: {
 			Violet: "hsl(257, 40%, 49%)",
 			SoftMagenta: "hsl(300, 69%, 71%)",
 		},
 		fontFamily: {
-			Montserrat: ["Poppins", "sans-serif"],
-			Fraunces: ["Open Sans", "serif"],
+			Poppins: ["Poppins", "sans-serif"],
+			OpenSans: ["Open Sans", "serif"],
 		},
 	},
 	plugins: [],
